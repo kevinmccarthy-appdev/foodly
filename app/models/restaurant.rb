51338f26d8c3bbeae24982226cdeae0eca2741df
Raghu Betina <raghu@firstdraft.com>
2018-05-30 15:51:35 +0000

    Validate presence of restaurants.zipcode

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,8 @@ class Restaurant < ApplicationRecord
 
   # Validations
 
+  validates :zipcode, :presence => { :message => "Can't be blank" }
+
   validates :zipcode, :length => { :minimum => 5, :maximum => 5 }
 
 end
