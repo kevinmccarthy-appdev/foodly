980c3e423ea44a318d21b00d3865288917c0689d
Raghu Betina <raghu@firstdraft.com>
2018-05-30 15:52:32 +0000

    Automatically associate comment to signed in user

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -47,8 +47,6 @@ class BookmarksController < ApplicationController
 
   def update
     @bookmark = Bookmark.find(params[:id])
-
-    @bookmark.user_id = params[:user_id]
     @bookmark.restaurant_id = params[:restaurant_id]
 
     save_status = @bookmark.save
