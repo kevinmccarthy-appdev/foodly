1c28a60a50f21fd1164adc4064274c328727f988
Raghu Betina <raghu@firstdraft.com>
2018-05-30 15:51:35 +0000

    Validate numericality of restaurants.price

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,8 @@ class Restaurant < ApplicationRecord
 
   # Validations
 
+  validates :price, :numericality => { :less_than_or_equal_to => 4, :greater_than_or_equal_to => 1 }
+
   validates :zipcode, :presence => { :message => "Can't be blank" }
 
   validates :zipcode, :length => { :minimum => 5, :maximum => 5 }
